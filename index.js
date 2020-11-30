04a41c0ed4258f1e140c2310e8f276ef4148182c
mtrimner <matt.trimner@gmail.com>
2020-11-30 10:56:42 -0800

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+superbowlWin = (records) => {
+  let wins = records.find(record => record.result === "W")
+  if (!!wins) {
+    return wins.year
+  } else {
+    return undefined
+  }
+}
